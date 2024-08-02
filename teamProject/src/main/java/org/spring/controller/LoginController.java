5bde2e12f72bcfc70fa863966b0cdd6a9de1998a
dlawlghks <dlawlghks202@naver.com>
2024-08-02 10:40:02 +0900

    url 쓸데없는거 지우기

--- teamProject/src/main/java/org/spring/controller/LoginController.java
+++ teamProject/src/main/java/org/spring/controller/LoginController.java
@@ -61,11 +61,11 @@ public class LoginController {
 			
 			session.setAttribute("user_info", user);
 			
-			redirectAttributes.addFlashAttribute("메세지","로그인 성공");
+			//redirectAttributes.addFlashAttribute("메세지","로그인 성공");
 			return "redirect:/user";
 		}else {
 			logger.info("로그인 실패");
-	        redirectAttributes.addFlashAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
+	        //redirectAttributes.addFlashAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
 	        return "redirect:/login";
 		}
 		
